8179ccbe7741dd672ff8236d487344c4a0bf5156
zainzhoucom <49436351+zainzhoucom@users.noreply.github.com>
2025-07-07 09:06:10 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.zainzhou.com"]: dockerHub,
+  ["quay.zainzhou.com"]: "https://quay.io",
+  ["gcr.zainzhou.com"]: "https://gcr.io",
+  ["k8s-gcr.zainzhou.com"]: "https://k8s.gcr.io",
+  ["k8s.zainzhou.com"]: "https://registry.k8s.io",
+  ["ghcr.zainzhou.com"]: "https://ghcr.io",
+  ["cloudsmith.zainzhou.com"]: "https://docker.cloudsmith.io",
+  ["ecr.zainzhou.com"]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging.zainzhou.com"]: dockerHub,
 };
 
 function routeByHosts(host) {
